46f3a12f4bdd5aeedab556d63a7bd68b5031942b
cosisaxis <kishanelewis2@gmail.com>
2022-12-09 18:55:46 -0500

    navbar-finished

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,46 +1,62 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
-import {FaBars, FaTimes} from 'react-icons/fa'
-import logo from "../../assets/logo.png"
-import './Navbar.css'
-
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
+import logo from "../../assets/logo.png";
+import "./Navbar.css";
 
 const Navbar = () => {
+  const [burgerClick, setBurgerClick] = useState(false);
+  const burgerClicking = () =>{
+    setBurgerClick(!burgerClick)
+  }
   return (
     <div className="header">
       <div className="container">
         <div className="navbar">
           <Link>
-          <img src={logo} alt="logo" width={100} />
+            <img src={logo} alt="logo" width={100} />
           </Link>
-          <ul className='navbar-menu'>
+          <ul className={burgerClick? "navbar-menu active" : "navbar-menu"}>
             <li>
-              <NavLink className="nav-item" to="/">Home</NavLink>
+              <NavLink className="nav-item" to="/">
+                Home
+              </NavLink>
             </li>
             <li>
-              <NavLink className="nav-item" to="/about">About</NavLink>
+              <NavLink className="nav-item" to="/about">
+                About
+              </NavLink>
             </li>
             <li>
-              <NavLink className="nav-item" to="/menu">Menu</NavLink>
+              <NavLink className="nav-item" to="/menu">
+                Menu
+              </NavLink>
             </li>
             <li>
-              <NavLink className="nav-item" to="/delivery">Delivery</NavLink>
+              <NavLink className="nav-item" to="/delivery">
+                Delivery
+              </NavLink>
             </li>
             <li>
-              <NavLink className="nav-item" to="/contact">Contact</NavLink>
+              <NavLink className="nav-item" to="/contact">
+                Contact
+              </NavLink>
             </li>
             <li>
               <button>Contact us</button>
             </li>
           </ul>
-          <div className='burger'>
-            <FaTimes size={20} style={{color:"#fff"}} />
-            <FaBars size={20} style={{color:"#fff"}} />
+          <div className="burger" onClick={burgerClicking}>
+            {burgerClick ? (
+              <FaTimes size={20} style={{ color: "#000" }} />
+            ) : (
+              <FaBars size={20} style={{ color: "000" }} />
+            )}
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
