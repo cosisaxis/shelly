01b0fa8837b697202e333451156b7e492f0a051d
cosisaxis <kishanelewis2@gmail.com>
2022-12-12 11:46:55 -0500

    changed-fonts

--- src/pages/Home/Hero/Hero.js
+++ src/pages/Home/Hero/Hero.js
@@ -1,15 +1,20 @@
 import React from 'react'
+
 import './Hero.css'
 
 const Hero = () => {
   return (
-    <div className='hero-image'>
+    <div className='hero-img container'>
         <div className='hero-content'>
-            <h1 className='hero-header'>
-                Welcome to <span className='hero-name'>Shelly's</span>
+            <h1 className='heading-primary'>
+                <span>welcome</span> to restaurant
             </h1>
-            <p className='hero-text'>The best home style cooking in jamaica</p>
-            <p className='hero-text'>You can contact us at <span className='hero-number'>1876-867-27-88</span></p>
+
+            <p className='text-white'>The best gourmet restaurant available in Manhatten</p>
+
+            <p className='text-white'>
+                Book online or call <span className='special-word'>(555)123-4567</span>
+            </p>
         </div>
     </div>
   )
