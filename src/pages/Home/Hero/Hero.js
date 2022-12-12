6f30ee886755ca5053740f44a1dd177e8143ae39
cosisaxis <kishanelewis2@gmail.com>
2022-12-12 11:51:18 -0500

    testing-6

--- src/pages/Home/Hero/Hero.js
+++ src/pages/Home/Hero/Hero.js
@@ -7,10 +7,10 @@ const Hero = () => {
     <div className='hero-img container'>
         <div className='hero-content'>
             <h1 className='heading-primary'>
-                <span>welcome</span> to restaurant
+                <span>welcome</span> to Shelly's
             </h1>
 
-            <p className='text-white'>The best gourmet restaurant available in Manhatten</p>
+            <p className='text-white'>The best home-style restaurant available in Jamaica</p>
 
             <p className='text-white'>
                 Book online or call <span className='special-word'>(555)123-4567</span>
