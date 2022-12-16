fd7dd660aa9794b93c3ac1c9d85cabe32398e79d
cosisaxis <kishanelewis2@gmail.com>
2022-12-16 13:51:48 -0500

    menu-header

--- src/pages/Menu/Menu.js
+++ src/pages/Menu/Menu.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import HeroImage from '../../components/HeroImage/HeroImage'
+import background from "../../assets/background.jpg"
 
 const Menu = () => {
   return (
-    <div>Menu</div>
+    <div>
+      <HeroImage 
+      bgImage = {background}
+      heading = {["Menu"]}
+      text = {"Take a look at our incredible menu"}
+    
+    
+      />
+    </div>
   )
 }
 
